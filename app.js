72efd62cd5fcd80eeffc205478e7513ed77fa236
Rodrigo Contreras <Rodrigolcontreras1317@gmail.com>
2022-06-18 13:19:24 -0300

    subida a heroku

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
- const Server = require(`./models/server`)
- const server = new Server();
- server.listen();    
+ const server = require(`./models/server`)
+ const servidor = new server();
+ servidor.listen();    
